b7905789f69cc876105bde1ade685f33a837c6be
Kruthik-ek <kruthikek631@gmail.com>
2026-09-11 10:19:57 +0530

    Add files via upload

---
+++ day4_assesment_tc2.java
@@ -0,0 +1,33 @@
+package seleniumassesment;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day4_assesment_tc2 {
+	public static void main (String args[]) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.zomato.com/bangalore/delivery");
+		driver.findElement(By.xpath("//button[text()='Log in']")).click();
+	
+		
+		WebElement frame1 = driver.findElement(By.id("auth-login-ui"));
+		driver.switchTo().frame(frame1);
+		driver.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("9937465345");
+		Thread.sleep(2000);
+		driver.switchTo().defaultContent();
+		
+		String text=driver.findElement(By.xpath("(//div[text()='Dining Out'])")).getText();
+		if(text.equals("Dining Out")) {
+		System.out.println("Main page");	
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+
+}
